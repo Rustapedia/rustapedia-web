88e438a4e9af157455155f861e52b098aef6b5b3
Anna Minasian <32515945+AnnaMinasian@users.noreply.github.com>
2019-06-09 01:08:47 +0300

    rename property in data

--- app/containers/App/data.js
+++ app/containers/App/data.js
@@ -3,7 +3,7 @@ export default {
     {
       id: 'qwerqwer',
       name: 'Bed',
-      img: 'bed',
+      shortName: 'bed',
       HP: 200,
       text:
         'The bed can be used as a spawn point, just like the sleeping bag, but it has reduced cooldown use; instead of waiting 5 minutes like the Sleeping Bag, you just wait 2 minutes.',
@@ -12,14 +12,14 @@ export default {
       id: 'lkjhsdlfkg',
       name: 'Chair',
       HP: 100,
-      img: 'chair',
+      shortName: 'chair',
       text:
         'The chair is normally made for decorative purpose. But you can mount it to receive 100% comfort. Hover over it and press the E key to mount it and press the space bar to dismount it.',
     },
     {
       id: 'zxcvzcxvzxcv',
       name: 'Bota Bag',
-      img: 'botabag',
+      shortName: 'botabag',
       text:
         'The Bota Bag is a handmade pouch for the collection and transportation of drinkable water.',
     },
@@ -27,7 +27,7 @@ export default {
       id: 'fnfnfnf',
       name: 'Rug Bear Skin',
       HP: 100,
-      img: 'rug.bear',
+      shortName: 'rug.bear',
       text:
         'Bear Rug skin that can be placed on most walls, floors, and ceilings. Note: If placed on a ceiling, standing directly below its head/neck will provide 100% comfort, even when alone.',
     },
@@ -35,14 +35,14 @@ export default {
       id: 'efekfekfe',
       name: 'Small Stash',
       HP: 150,
-      img: 'stash.small',
+      shortName: 'stash.small',
       text:
         'Small stashes are a very useful tool in the early game, especially when saving enough resources to build a starter base, or hiding your key to your door.',
     },
     {
       id: 'rhtjj',
       name: 'Repair Bench',
-      img: 'box.repair.bench',
+      shortName: 'box.repair.bench',
       HP: 200,
       Decay: '8 hours',
       text:
@@ -51,7 +51,7 @@ export default {
     {
       id: 'rjtjtjtjt',
       name: 'Christmas Tree',
-      img: 'xmas.tree',
+      shortName: 'xmas.tree',
       HP: 1000,
       Decay: '96 hours',
       text:
@@ -62,7 +62,7 @@ export default {
       name: 'Jack O Lantern Angry',
       HP: 50,
       Decay: '48 hours',
-      img: 'jackolantern.angry',
+      shortName: 'jackolantern.angry',
       text: 'A Lantern. Place it where you need light.',
     },
   ],
@@ -70,21 +70,21 @@ export default {
     {
       id: 'fghjfghjfghj',
       name: 'Assault Rifle',
-      img: 'rifle.ak',
+      shortName: 'rifle.ak',
       text:
         'The Assault Rifle is an accurate, powerful, and fully automatic rifle that fires 5.56 rifle rounds. It has a moderate rate of fire which allows for proficiency at close to medium range.',
     },
     {
       id: 'ertyoertywte',
       name: 'M249',
-      img: 'm249',
+      shortName: 'm249',
       text:
         'A military-grade Light Machine Gun that only spawns in helicopter crates and Bradly APC, the M249 holds the most bullets in-game out of all the guns, with a magazine capacity of 100. Does an extremely high amount of damage with a high rate of fire and is extremely accurate, but has a large amount of recoil. Melts Everybody.',
     },
     {
       id: 'qwerqwertyy',
       name: 'Semi-Automatic Rifle',
-      img: 'rifle.semiauto',
+      shortName: 'rifle.semiauto',
       text:
         'The Semi-Automatic Rifle is a staple of low quality weapons due to its high cost-efficiency. With its medium-tier damage, comparatively low recoil and high accuracy, the Semi-Automatic Rifle is the jack of all trades, but master of none.',
     },
@@ -93,21 +93,21 @@ export default {
     {
       id: 'sdfgsdfghu7u7urtv',
       name: 'Apple',
-      img: 'apple',
+      shortName: 'apple',
       text:
         'The apple is a food that gives the player food and water. Unlike most other foods the apple gives lots more hydration when consumed. Can be found in food crates and medical crates.',
     },
     {
       id: 'htjtjtjyj',
       name: 'Chocholate Bar',
-      img: 'chocholate',
+      shortName: 'chocholate',
       text:
         'Chocolate Bar found as loot. Eating it provides a small boost to health, hunger, and thirst.',
     },
     {
       id: 'hrhrhrhr',
       name: 'Mushroom',
-      img: 'mushroom',
+      shortName: 'mushroom',
       text:
         "Mushrooms are very helpful food items that can be found in forest and snow biomes. Although they don't provide much food they can be found in abundance in brown areas viable in the in-game map.",
     },
